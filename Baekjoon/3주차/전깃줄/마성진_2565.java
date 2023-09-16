9ed0a1915a58bb7a6cdbeac4030a85d7e4b1d98c
MaSeongJin <73871625+MaSeongJin@users.noreply.github.com>
2023-09-16 18:50:51 +0900

    Create 마성진_2565.java

---
+++ Baekjoon/3주차/전깃줄/마성진_2565.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+	static int[] dp;
+	static int[][] wire;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		dp = new int[N];
+		wire = new int[N][2];
+
+		StringTokenizer st;
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			wire[i][0] = Integer.parseInt(st.nextToken());
+			wire[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(wire, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[0] - o2[0];
+			}
+		});
+
+		int max = 0;
+
+		for (int i = 0; i < N; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (wire[i][1] > wire[j][1]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+			max = Math.max(dp[i], max);
+		}
+
+		System.out.println(N - max);
+
+	}
+
+}
