f8b892dd7bc46aa9bb8ff4696b2448b3b7450f5c
Moon Jun Hyeong <114069644+NoRuTnT@users.noreply.github.com>
2023-09-20 21:04:58 +0900

    Update 문준형_1711.java

--- Baekjoon/1주차/직각삼각형/문준형_1711.java
+++ Baekjoon/1주차/직각삼각형/문준형_1711.java
@@ -6,24 +6,23 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class b1711 {
-	static int[][] point;
+	static long[][] point;
 	static int n,cnt;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;		
 		n=Integer.parseInt(br.readLine());
-		point = new int[n][2];
+		point = new long[n][2];
 		cnt=0;
 		for(int i=0;i<n;i++) {
 			st= new StringTokenizer(br.readLine());
-			point[i][0]=Integer.parseInt(st.nextToken());
-			point[i][1]=Integer.parseInt(st.nextToken());			
+			point[i][0]=Long.parseLong(st.nextToken());
+			point[i][1]=Long.parseLong(st.nextToken());			
 		}
-		int len = point.length;
-        for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j++) {
-                for (int k = j + 1; k < len; k++) {
+        for (int i = 0; i < n-2; i++) {
+            for (int j = i + 1; j < n-1; j++) {
+                for (int k = j + 1; k < n; k++) {
                     if (triangle(point[i], point[j], point[k])) {                 
                         cnt++;                        
                     }
@@ -33,21 +32,21 @@ public class b1711 {
         
         System.out.println(cnt);
 	}
-    static boolean triangle(int[] a, int[] b, int[] c) {
+    static boolean triangle(long[] a, long[] b, long[] c) {
         // 각 점으로부터 다른 두 점 사이의 벡터를 계산
-        int[] ab = new int[] { b[0]-a[0], b[1]-a[1] };
-        int[] ac = new int[] { c[0]-a[0], c[1]-a[1] };
-        int[] bc = new int[] { c[0]-b[0], c[1]-b[1] };
+    	long[] ab = new long[] { b[0]-a[0], b[1]-a[1] };
+    	long[] ac = new long[] { c[0]-a[0], c[1]-a[1] };
+    	long[] bc = new long[] { c[0]-b[0], c[1]-b[1] };
 
         // 벡터의 내적을 계산
-        int dotABAC = ab[0]*ac[0] + ab[1]*ac[1];
-        int dotABBC = ab[0]*bc[0] + ab[1]*bc[1];
-        int dotACBC = ac[0]*bc[0] + ac[1]*bc[1];
+        long dotABAC = ab[0]*ac[0] + ab[1]*ac[1];
+        long dotABBC = ab[0]*bc[0] + ab[1]*bc[1];
+        long dotACBC = ac[0]*bc[0] + ac[1]*bc[1];
 
         // 내적이 0이면 직각 삼각형
         return (dotABAC == 0) || (dotABBC == 0) || (dotACBC == 0);
-    }    
-
+    }
+	    
 }
 	
 
