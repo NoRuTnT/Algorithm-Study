2ea3e199801ba3c1c39806fe7944dff3f6ea2a90
Moon Jun Hyeong <114069644+NoRuTnT@users.noreply.github.com>
2023-09-20 21:05:38 +0900

    Update 문준형_14502.java

--- Baekjoon/1주차/연구소/문준형_14502.java
+++ Baekjoon/1주차/연구소/문준형_14502.java
@@ -3,15 +3,21 @@ package gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
-
+class Node{
+	int x;
+	int y;
+	Node(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+}
 public class b14502 {
 	static int n,m;
 	static int[][] map;
-	static boolean[][]visit;
-	static boolean[][]visit2, twocheck;
+	public static int maxSafe = Integer.MIN_VALUE;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,82 +31,71 @@ public class b14502 {
 				map[i][j]=Integer.parseInt(st.nextToken());
 			}
 		}
-		visit = new boolean[n][m];		
-		twocheck=new boolean[n][m];
 		dfs(0);
-		int cnt=0;
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<m;j++) {
-				if(map[i][j]==0) {
-					cnt++;
-				}
-
-			}
-		}
-		System.out.println(Arrays.deepToString(map));
-		System.out.println(cnt);
+		System.out.println(maxSafe);		
 
 	}
 	//기둥3개두기
 	public static void dfs(int depth) {
 		if(depth==3) {
-			for(int i=0;i<n;i++) {
-				for(int j=0;j<m;j++) {
-					if(map[i][j]==2&&!twocheck[i][j]) {
-						map[i][j]=0;
-					}
-				}				
-			}
-			visit2 = new boolean[n][m];
-			for(int i=0;i<n;i++) {
-				for(int j=0;j<m;j++) {
-					if(map[i][j]==2 && !visit2[i][j]) {
-						twocheck[i][j]=true;
-						visit2[i][j]=true;
-						dfs1(i,j);						
-					}
-				}
-			}
+			bfs();
 			return;
 		}
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<m;j++) {
-				if(!visit[i][j] && map[i][j]==0) {
+				if(map[i][j]==0) {
 					map[i][j]=1;
-					visit[i][j]=true;
 					dfs(depth+1);
-					visit[i][j]=false;
 					map[i][j]=0;
 				}
 			}
 		}
 	}
 	//바이러스확산
-	private static void dfs1(int i,int j) {
+	private static void bfs() {
 		int[] dr = {0,1,0,-1};
 		int[] dc = {1,0,-1,0};
-		visit2[i][j]=true;
-		Stack<int[]> stack= new Stack<>();
-		stack.add(new int[] {i,j});
-		while(!stack.isEmpty()) {
-			int[] now = stack.pop();
-			for(int a=0;a<4;a++) {
-				int r = now[0]+dr[a];
-				int c = now[1]+dc[a];
-				if(r>=0 && r<n && c>=0 && c<n) {
-					if(map[r][c]==0 && !visit2[r][c]) {
-						map[r][c]=2;
-						stack.add(new int[] {r,c});
-						visit2[r][c]=true;
+		Queue<Node> q = new LinkedList<>();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] == 2) {
+					q.add(new Node(i, j));
+				}
+			}
+		}
+		int copyMap[][] = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			copyMap[i] = map[i].clone();
+		}
+		while (!q.isEmpty()) {
+			Node now = q.poll();
+			int x = now.x; 
+			int y = now.y;
+			for (int k=0; k<4;k++) {
+				int nx = x + dr[k];
+				int ny = y + dc[k];
+				if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+					if (copyMap[nx][ny] == 0) {
+						q.add(new Node(nx, ny));
+						copyMap[nx][ny] = 2;
 					}
 				}
+			}
+			
+		}
+		funcSafeZone(copyMap);
+	}
+			
+		private static void funcSafeZone(int[][] copyMap) {
+			int safeZone = 0;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					if (copyMap[i][j] == 0) {
+						safeZone++;
+					}
+				}
+			}
+			maxSafe = Math.max(maxSafe, safeZone);
 		}
 
-	            }
-	        }
-
-
-		}
-
-
-
+	}
