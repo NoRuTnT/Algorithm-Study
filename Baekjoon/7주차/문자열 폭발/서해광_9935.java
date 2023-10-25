31e41c02054e31a3c965bdb03e138cbc682e44df
hks816 <143579622+hks0704@users.noreply.github.com>
2023-10-25 16:57:17 +0900

    Update 서해광_9935.java

--- Baekjoon/7주차/문자열 폭발/서해광_9935.java
+++ Baekjoon/7주차/문자열 폭발/서해광_9935.java
@@ -2,7 +2,7 @@
 // 스택 안씀
 import java.util.Scanner;
 
-public class Main8 {
+public class Main {
 	public static String sentence, key;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
